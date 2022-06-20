75bcf367555639fd79fd9e50ea44fe6625bc86fa
lamouchedu94 <lamouchedu54@gmail.com>
2022-06-20 22:43:37 +0200

    Affiche Date Photo

--- main.go
+++ main.go
@@ -3,22 +3,51 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"log"
+	"os"
 	"path/filepath"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
 )
 
 func main() {
-	chemin := "/home"
+	chemin := "./Photos"
 
 	err := filepath.Walk(chemin, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(path)
+		if !info.IsDir() {
+			date_img(path)
+		}
+		//fmt.Println(path)
+
 		return nil
 	})
 	if err != nil {
 		fmt.Println(err)
 	}
 
+}
+func date_img(fname string) {
+
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*
+		camModel, _ := x.Get(exif.Model)
+		fmt.Println(camModel.StringVal())
+	*/
+	tm, _ := x.DateTime()
+	fmt.Println(fname, tm)
 }
