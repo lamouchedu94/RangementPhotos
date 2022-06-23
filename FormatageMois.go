363ff347b306718583393c28e30f4ecf0ad99b72
lamouchedu94 <lamouchedu54@gmail.com>
2022-06-23 10:51:23 +0200

    correction bug Mois > 10

--- FormatageMois.go
+++ FormatageMois.go
@@ -7,10 +7,11 @@ import (
 func FormatageMois(TabDate []string) []string {
 	TabMois := [12]string{"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"}
 	mois := TabDate[1]
-	if len(mois) == 2 {
+	if mois[0] == 0 {
 		mois = string(mois[1])
 	}
 	moisInt, _ := strconv.Atoi(mois)
+	//fmt.Println(TabDate)
 	TabDate[1] = TabMois[moisInt-1]
 
 	return TabDate
